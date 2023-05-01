a113b4b93fa97f5b6cd3369d1fd8e4c772d7edfa
DanielBzz <daniel6966@gmail.com>
2023-05-01 15:53:17 +0300

    adding two more algos and fix flake8 errors

--- stockAnalysis/thirdPartUtils/IndicatorsAlgo.py
+++ stockAnalysis/thirdPartUtils/IndicatorsAlgo.py
@@ -1,7 +1,9 @@
 import enum
 import sys
-from pandas import DataFrame
+import pandas
 import Yfinance as Yf
+import numpy as np
+from sklearn.linear_model import LinearRegression
 
 class Indicators(enum.Enum):
     RSI = 'rsi_algo'
@@ -13,18 +15,19 @@ class Indicators(enum.Enum):
     FORCE = 'force_algo'
 
 
-def calculate_algorithms(chosen_algos_list,stock_df):
+def calculate_algorithms(chosen_algos_list, stock_df):
     result = {}
     for alg in chosen_algos_list:
         for indicator in Indicators:
             if indicator.name == alg:
-                result[indicator.name] = getattr(sys.modules.get(__name__),indicator.value)(stock_df)
+                result[indicator.name] = getattr(sys.modules.get(__name__), indicator.value)(stock_df)
                 break
 
     return result
 
+
 def rsi_algo(stock_df):
-    delta = DataFrame(stock_df[Yf.CLOSE_PRICE].diff())
+    delta = pandas.DataFrame(stock_df[Yf.CLOSE_PRICE].diff())
     gains = delta.where(delta > 0, 0)
     losses = -delta.where(delta < 0, 0)
     avg_gains = gains.rolling(window=14).mean()
@@ -35,3 +38,25 @@ def rsi_algo(stock_df):
 
     return rsi
 
+
+def bollinger_algo(stock_df):
+    rolling_mean = stock_df[Yf.CLOSE_PRICE].rolling(window=14).mean()
+    bolinger = (stock_df[Yf.CLOSE_PRICE] - rolling_mean)**2
+    rolling_std = np.sqrt(bolinger.rolling(window=14).sum())
+    upper_band = rolling_mean + (rolling_std * 2)
+    lower_band = rolling_mean - (rolling_std * 2)
+
+    return rolling_mean, upper_band, lower_band
+
+
+def linear_reg_algo(stock_df):
+    stock_df.dropna(inplace=True)
+    stock_df.index = pandas.to_datetime(stock_df.index, format='%Y-%m-%d')
+    x = np.array(range(1, len(stock_df) + 1)).reshape((-1, 1))
+    y = stock_df[Yf.CLOSE_PRICE].values
+    model = LinearRegression()
+    model.fit(x, y)
+    future_dates = np.array(range(len(stock_df) + 1, len(stock_df) + 11)).reshape((-1, 1))
+    future_prices = model.predict(future_dates)
+
+    return future_prices
