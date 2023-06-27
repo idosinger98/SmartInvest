61afb7a0b60a4c2be506920ea529d451a5a3d54d
DanielBzz <daniel6966@gmail.com>
2023-06-27 13:16:05 +0300

    add model to db of analyzed stocks and add manager of queries

--- stockAnalysis/models.py
+++ stockAnalysis/models.py
@@ -1,3 +1,18 @@
-# from django.db import models
+from django.db import models
+from users.models import Profile
 
-# Create your models here.
+class AnalyzedStocksManager(models.Manager):
+
+    def get_user_stocks(self,analyst_id):
+        return self.filter(analyst_id__user_id=analyst_id)
+
+    def get_community_stocks(self):
+        return self.filter(is_public=True)
+
+
+class AnalyzedStocks(models.Model):
+    analyst_id = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    stock_image = models.JSONField() # for now as json. maybe will change.
+    description = models.TextField(blank=True)
+    is_public = models.BooleanField(default=False)
+    objects = AnalyzedStocksManager()
