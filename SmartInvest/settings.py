9c60fa2b366790675a51d544e4f448be90fba4ab
Ido Singer <idosinger1998@gmail.com>
2023-06-30 12:02:58 +0300

    Add review feature and tests
    The changes made:
    
    1.Add a page for create a review.
    
    2.Add tests for the review view
    
    3.Add style_review.css

--- SmartInvest/settings.py
+++ SmartInvest/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users.apps.UsersConfig',
+    'review.apps.ReviewConfig',
+    'landingPage.apps.LandingpageConfig',
 ]
 
 MIDDLEWARE = [
---
+++ review/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from review.forms import ReviewForm
+from review.models import Review
+from users.models import Profile
+from datetime import datetime
+from django.contrib import messages
+
+
+@login_required
+def createReviewView(request):
+    profile = Profile.objects.filter(user_id=request.user)[0]
+    if request.method == 'GET':
+        form = ReviewForm()
+        return render(request, 'review/create_review.html', {'form': form})
+    elif request.method == 'POST':
+        form = ReviewForm(request.POST)
+        if Review.objects.filter(publisher_id=profile):
+            messages.error(request, "You have already given a review on the site")
+        elif form.is_valid():
+            review = Review.objects.create(publisher_id=profile, content=form.cleaned_data['content'],
+                                           rating=form.cleaned_data['rating'], date=datetime.now())
+            review.save()
+            return redirect('change_password')
+        else:
+            messages.error(request, "You must enter rating")
+    return render(request, 'review/create_review.html', {'form': form})
