e7c49dbfac8d16e61f1aa364775c8d4ee26a2f00
idosinger98 <118663500+idosinger98@users.noreply.github.com>
2023-07-12 15:00:45 +0300

    Password Reset and changed login page design (#22)
    * Password Reset and changed login.html
    
    use email to send a password reset link to a user
    -Users will be able to fill out a form with their email and have a unique token sent to them,
    and if their token is verified then they will be able to create a new password.
    
    * changed register page design

--- users/views.py
+++ users/views.py
@@ -2,11 +2,26 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth import login, authenticate, logout
 from users.forms import LoginForm, RegisterForm, UpdateProfileForm, PasswordChangingForm, UserUpdateForm, ProfileForm
+from users.forms import ResetPasswordForm
 from django.contrib.auth.views import PasswordChangeView
 from users.models import Profile
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.decorators import login_required
+import sib_api_v3_sdk
+from sib_api_v3_sdk.rest import ApiException
+from django.utils.http import urlsafe_base64_encode
+from django.contrib.auth.tokens import default_token_generator
+from django.utils.encoding import force_bytes
+from django.template.loader import render_to_string
+from django.contrib.auth.models import User
+from django.db.models.query_utils import Q
+from django.http import HttpResponse
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 
 def sign_up_view(request):
@@ -84,3 +99,63 @@ def edit_profile(request):
         'profile_form': profile_form
     }
     return render(request, 'users/edit_profile.html', context)
+
+
+def reset_password(request):
+    if request.method == 'GET':
+        form = ResetPasswordForm()
+        return render(request, 'users/password_reset.html', {'form': form})
+
+    if request.method == 'POST':
+        form = ResetPasswordForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data['email']
+            return send_email(request, email, "Password Reset Requested", 'users/password_reset_email.html',
+                              'users/reset_password_done.html', False, form)
+        else:
+            # Handle form validation errors
+            return render(request, 'users/password_reset.html', {'form': form})
+
+
+def send_email(request, email, subject_str, email_template_html, message, flag, form):
+    associated_users = User.objects.filter(Q(email=email))
+    for user in associated_users:
+        if user.is_active or flag:
+            email_template_name = email_template_html
+            c = {
+                "email": user.email,
+                'domain': '127.0.0.1:8000',
+                'site_name': 'Website',
+                "uid": urlsafe_base64_encode(force_bytes(user.pk)),
+                "user": user,
+                'token': default_token_generator.make_token(user),
+                'protocol': 'http',
+            }
+            email = render_to_string(email_template_name, c)
+
+            # Configure API key authorization: api-key
+            configuration = sib_api_v3_sdk.Configuration()
+            configuration.api_key[
+                'api-key'] = os.environ.get('MAIL_KEY')
+
+            # create an instance of the API class
+            api_instance = sib_api_v3_sdk.TransactionalEmailsApi(sib_api_v3_sdk.ApiClient(configuration))
+
+            subject = subject_str
+            html_content = email
+            sender = {"name": 'Smart Invest', "email": 'smartinvest850@gmail.com'}
+            to = [{"email": user.email, "name": 'Daniell'}]
+            headers = {"Some-Custom-Name": "unique-id-1234"}
+            send_smtp_email = sib_api_v3_sdk.SendSmtpEmail(to=to, headers=headers, html_content=html_content,
+                                                           sender=sender, subject=subject)
+            try:
+                api_instance.send_transac_email(send_smtp_email)
+                messages.success(request, "Email send successfully")
+            except ApiException:
+                return HttpResponse('Invalid header found.')
+
+            return render(request, message)
+
+        else:
+            messages.error(request, 'The email is not found in the system')
+            return render(request, 'users/password_reset.html', {'form': form})
