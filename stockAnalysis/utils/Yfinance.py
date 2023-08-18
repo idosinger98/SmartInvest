b4c51558a93c9f7d60ad21d061e8440f12732532
TalReinfeld <talreinfeld1@gmail.com>
2023-08-18 19:04:48 +0300

    System can fetch stocks and indices
    Signed-off-by: TalReinfeld <talreinfeld1@gmail.com>

--- stockAnalysis/utils/Yfinance.py
+++ stockAnalysis/utils/Yfinance.py
@@ -14,6 +14,39 @@ LOW_PRICE = 'Low'
 OPEN_PRICE = 'Open'
 VOLUME = 'Volume'
 
+INDICES = {'Open': 'regularMarketOpen',
+           'Previous Close': 'regularMarketPreviousClose',
+           'Volume': 'volume',
+           'Avg. Volume': 'averageVolume'}
+
+STOCK_RATIOS = {'Current Ratio': ['currentRatio',
+                                  "The current ratio is a financial metric used to evaluate"
+                                  " a company's short-term liquidity and ability to cover its"
+                                  " short-term liabilities with its short-term assets."],  # 15%
+                'Quick Ratio': ['quickRatio',
+                                "The quick ratio, also known as the acid-test ratio, is a financial metric"
+                                " used to assess a company's short-term liquidity and its ability to cover"
+                                " immediate liabilities without relying on the sale of inventory."],  # 10%
+                'Gross Profit Margin': ['grossMargins',
+                                        "Gross Profit Margin is a financial metric used to evaluate a"
+                                        " company's profitability and efficiency in generating profit"
+                                        " from its core business operations."],  # 20%
+                'Short Ratio': ['shortRatio',
+                                "The short ratio, also known as the short interest ratio or days"
+                                " to cover ratio, is a financial metric used to assess the level"
+                                " of short interest in a particular stock."],  # 5%
+                'Price/Earning to Growth': ['pegRatio',
+                                            "The Price/Earnings to Growth ratio, often abbreviated as PEG ratio,"
+                                            " is a valuation metric used in finance to assess the relationship"
+                                            " between a company's stock price, its earnings per share (EPS),"
+                                            " and its expected growth rate."]}  # 25%
+
+STOCK_FORMULAS = {'Price-to-Earning (P/E) ratio': ['pegRatio',
+                                                   'earningsGrowth',
+                                                   "The Price-to-Earnings (P/E) ratio is a financial metric used to"
+                                                   " assess the relative valuation of a company's stock by comparing"
+                                                   " its market price per share to its earnings per share (EPS)."]}  # 25%
+
 
 def get_last_price_stock(stock_name):
     ticker = yf.Ticker(stock_name).history()
@@ -54,17 +87,13 @@ def __check_start_date_before_end(start, end):
 
 
 def get_stock_fundamentals(stock_name):
-    response = yf.Ticker(stock_name).get_info()
+    response = yf.Ticker(stock_name).info
 
-    built_in_ratios = {'Current Ratio': 'currentRatio',
-                       'Quick Ratio': 'quickRatio',
-                       'Gross Profit Margin': 'grossMargins',
-                       'Short Ratio': 'shortRatio',
-                       'Price/Earning to Growth': 'pegRatio',
-                       }
-    not_built_in_ratios = {'Price-to-Earning (P/E) ratio': ['pegRatio', 'earningsGrowth']}
+    if stock_name[0] == '^':
+        total_ratios = dict(map(lambda item: (item[0], response[item[1]]), INDICES.items()))
+    else:
+        total_ratios = dict(map(lambda item: (item[0], response[item[1][0]]), STOCK_RATIOS.items()))
+        total_ratios.update(dict(map(lambda item: (item[0], response[item[1][0]] * response[item[1][1]]),
+                                     STOCK_FORMULAS.items())))
 
-    total_ratios = dict(map(lambda item: (item[0], response[item[1]]), built_in_ratios.items()))
-    total_ratios.update(dict(map(lambda item: (item[0], response[item[1][0]] * response[item[1][1]]),
-                                 not_built_in_ratios.items())))
     return total_ratios
