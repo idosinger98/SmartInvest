3b4279e3d43b3f28468da52a5f893472ff70f2dd
TalReinfeld <talreinfeld1@gmail.com>
2023-08-29 00:37:52 +0300

    Design of fundamentals compare
    Signed-off-by: TalReinfeld <talreinfeld1@gmail.com>

--- stockAnalysis/views.py
+++ stockAnalysis/views.py
@@ -89,14 +89,9 @@ def compare_stocks(request):
 
 
 def get_stock_fundamentals_score(fundamentals):
-    score = float(fundamentals['Current Ratio']) * 0.25 +\
-            float(fundamentals['Quick Ratio']) * 0.1 +\
-            float(fundamentals['Gross Profit Margin']) * 0.2 +\
-            float(fundamentals['Short Ratio']) * 0.05 +\
-            float(fundamentals['Price/Earning to Growth']) * 0.25 +\
-            float(fundamentals['Price-to-Earning (P/E) ratio']) * 0.25
+    scaled_data = {key: float(value) * (1 / len(fundamentals)) for key, value in fundamentals.items()}
 
-    return score
+    return sum(scaled_data.values())
 
 
 def new_stock_is_better(fundamentals, fundamentals_items):
